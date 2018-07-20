028ab006857dd33e0c7d4b64af126deaede7200f
petrozavodsky <petrozavodsky@gmail.com>
2018-07-20 13:19:28 +0300

    initial

---
+++ ses.php
@@ -0,0 +1,31 @@
+<?php
+$SES = array (
+  '1c61b6904f1073784017f332293372e2' => 
+  array (
+    'cfg' => 
+    array (
+      'charsets' => 'cp1251 utf8 latin1',
+      'lang' => 'ru',
+      'time_web' => '600',
+      'time_cron' => '600',
+      'backup_path' => 'backup/',
+      'backup_url' => 'backup/',
+      'only_create' => 'MRG_MyISAM MERGE HEAP MEMORY',
+      'globstat' => 0,
+      'my_host' => 'localhost',
+      'my_port' => 3306,
+      'my_user' => 'heroine',
+      'my_pass' => 'jwgEJ70ADADZ',
+      'my_comp' => 0,
+      'my_db' => '',
+      'auth' => 'mysql cfg',
+      'user' => '',
+      'pass' => '',
+      'confirm' => '6',
+      'exitURL' => './',
+    ),
+    'time' => 1520243379,
+    'lng' => 'ru',
+  ),
+);
+?>
